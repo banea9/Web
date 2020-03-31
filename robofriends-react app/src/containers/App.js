d5e79dc8b0cc59ec712516a2188997b1a6134608
banea9 <banea9@gmail.com>
2020-03-31 13:56:12 +0200

    robofriends adding redux

--- robofriends-react app/src/containers/App.js
+++ robofriends-react app/src/containers/App.js
@@ -1,43 +1,86 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
 import SearchBox from "../components/SearchBox";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
-// import { robots } from "./robots";
+import { setSearchField, requestRobots } from "../actions";
 
+// import { robots } from "./robots";
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  };
+};
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: ""
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: []
+  //     //searchField: ""
+  //   };
+  // }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => {
-        return response.json();
-      })
-      .then(users => {
-        this.setState({ robots: users });
-      });
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then(response => {
+    //     return response.json();
+    //   })
+    //   .then(users => {
+    //     this.setState({ robots: users });
+    //   });
+    this.props.onRequestRobots();
   }
 
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
-  };
+  // onSearchChange = event => {
+  //   this.setState({ searchField: event.target.value });
+  // };
+
+  //-------------Without Redux-------------
+
+  // render() {
+  //   const { robots, searchField } = this.state;
+  //   const filteredRobots = robots.robots.filter(robot => {
+  //     return robot.name
+  //       .toLowerCase()
+  //       .includes(searchField.searchField.toLowerCase());
+  //   });
+  //   return (
+  //     <div className="tc">
+  //       <h1 className="f2">RoboFriends</h1>
+  //       <SearchBox searchChange={this.onSearchChange} />
+  //       <Scroll>
+  //         <ErrorBoundary>
+  //           <CardList robots={filteredRobots} />
+  //         </ErrorBoundary>
+  //       </Scroll>
+  //     </div>
+  //   );
+  // }
+  // ------------------------------------
+
+  //With Redux
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
+    const { searchField, onSearchChange, robots } = this.props;
+    const filteredRobots = robots.filter(robot => {
       return robot.name
         .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+        .includes(searchField.toLowerCase());
     });
     return (
       <div className="tc">
         <h1 className="f2">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filteredRobots} />
@@ -48,4 +91,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- robofriends-react app/src/index.js
+++ robofriends-react app/src/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { searchRobots, requestRobots } from "./reducers";
+import thunkMiddleware from "redux-thunk";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
 import "./index.css";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 
-ReactDOM.render(<App /> , document.getElementById("root"));
+const logger = createLogger(); //redux middleware
+const rootReducer = combineReducers({searchRobots, requestRobots}) 
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+); // we use here searchRobots for our reducer, but in projects with multiple reducers we need to combine them all into on single root reducer and pass this reducer to the createStore() funciton.
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
